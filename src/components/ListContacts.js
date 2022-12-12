5db6e4387dd7e30c64799bb1f60224408829c541
ifaizanahmedk <faizanahmed497@gmail.com>
2022-12-13 01:41:02 +0500

    FEAT: Add Search using Controlled Components to ListContacts.js.

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,27 +1,72 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const ListContacts = ({ contacts, onDeleteContact }) => {
+	const [query, setQuery] = useState("");
+
+	const updateQuery = (query) => {
+		setQuery(query.trim());
+	};
+
+	const clearQuery = () => {
+		updateQuery("");
+	};
+
+	const showingContacts =
+		query === ""
+			? contacts
+			: contacts.filter((c) =>
+					c.name.toLowerCase().includes(query.toLowerCase())
+			  );
+
 	return (
-		<ol className="contact-list">
-			{contacts.map((contact) => (
-				<li key={contact.id} className="contact-list-item">
-					<div
-						className="contact-avatar"
-						style={{
-							backgroundImage: `url(${contact.avatarURL})`,
-						}}></div>
-					<div className="contact-details">
-						<p>{contact.name}</p>
-						<p>{contact.handle}</p>
-					</div>
+		<div className="list-contacts">
+			<div className="list-contacts-top">
+				<input
+					className="search-contacts"
+					type="text"
+					placeholder="Search Contacts"
+					value={query}
+					onChange={(event) => updateQuery(event.target.value)}
+				/>
+			</div>
+
+			{showingContacts.length !== contacts.length && (
+				<div className="showing-contacts">
+					<span>
+						Now showing {showingContacts.length} of&nbsp;
+						{contacts.length}
+					</span>
 					<button
-						className="contact-remove"
-						onClick={() => onDeleteContact(contact)}>
-						Remove
+						onClick={() => {
+							clearQuery();
+						}}>
+						Show All
 					</button>
-				</li>
-			))}
-		</ol>
+				</div>
+			)}
+
+			<ol className="contact-list">
+				{showingContacts.map((contact) => (
+					<li key={contact.id} className="contact-list-item">
+						<div
+							className="contact-avatar"
+							style={{
+								backgroundImage: `url(${contact.avatarURL})`,
+							}}></div>
+						<div className="contact-details">
+							<p>{contact.name}</p>
+							<p>{contact.handle}</p>
+						</div>
+						<button
+							className="contact-remove"
+							onClick={() => onDeleteContact(contact)}>
+							Remove
+						</button>
+					</li>
+				))}
+			</ol>
+		</div>
 	);
 };
 
